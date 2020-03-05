30ea1c2c83c6087f1aa6097b77b0373b5d7bee4a
Jordan Conrad <Jordan.Conrad@hyland.com>
2020-03-04 19:15:16 -0500

    Movie List

---
+++ Week 4 code along/Week 4 code along/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week_4_code_along
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Movie (string myTitle, int myReleaseYear)
+        {
+            Title = myTitle;
+            ReleaseYear = myReleaseYear; 
+        }
+
+        public void ShowDetails()
+        {
+            string info = "Title: " + Title;
+            info += "\nRelease Year: " + ReleaseYear;
+            MessageBox.Show(info);
+        }
+    }
+}
